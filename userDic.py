89599e17f2edb310dd4768ae13427110db9ae828
chaitanyapentakota <117889524+chaitanyapentakota@users.noreply.github.com>
2022-11-14 11:14:18 +0530

    Create userDic.py

---
+++ userDic.py
@@ -0,0 +1,7 @@
+def people():
+    people = {1: {'name': 'John', 'age': '27', 'sex': 'Male','EmpId':'9091'},
+              2: {'name': 'Marie', 'age': '25', 'sex': 'Female','EmpId':'9092'},
+              3: {'name': 'chaitanya', 'age':'23','sex': 'male','EmpId':'9093'},
+              4: {'name': 'lahari', 'age': '22', 'sex': 'Female','EmpId':'9094'}}
+    print(people)          
+people()
