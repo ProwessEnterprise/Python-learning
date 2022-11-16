0a6ed587cceb4d7fc133223dd887e991b4613b3c
chaitanyapentakota <117889524+chaitanyapentakota@users.noreply.github.com>
2022-11-16 15:32:37 +0530

    Create tuple_string.py

---
+++ tuple_string.py
@@ -0,0 +1,7 @@
+def TupleToString(my_tuple):
+        s = ''.join(my_tuple)
+        return s
+Tuple = ('C', 'H', 'A', 'I', 'T', 'U')
+print("Before converting to String:", Tuple)
+str = TupleToString(Tuple)
+print("After converting to String:", str)
