ed639b0651918f9552b3d223c1c24b9415b29570
chaitanyapentakota <117889524+chaitanyapentakota@users.noreply.github.com>
2022-11-18 15:53:13 +0530

    Create count_list.py

---
+++ count_list.py
@@ -0,0 +1,11 @@
+def get_len(l):
+    return len(l)
+ 
+if __name__ == '__main__':
+    l1 = []         
+    l2 = [5,43,6,1]         
+    l3 = [[4,3],[0,1],[3]]  
+ 
+    print("L1 len: ", get_len(l1))
+    print("L2 len: ", get_len(l2))
+    print("L3 len: ", get_len(l3))
